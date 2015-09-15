57ef00080cfa33f82b9c247370ad9fa33d0342d4
Kyshnerev <kyshnerev@lightsoft.ru>
2015-09-16 00:17:05 +0300

    AMOP-9 Настроить страницу ошибок

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -23,15 +23,6 @@ class SiteController extends BaseController
         ];
     }
 
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ]
-        ];
-    }
-
     public function actionIndex()
     {
         return $this->render('index');
